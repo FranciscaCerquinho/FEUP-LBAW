37ade4be40393c74ab1df1258f5e0a7693c31569
FranciscaCerquinho <franciscacerquinho@gmail.com>
2018-04-12 13:03:08 +0100

    fixing bugs

--- app/Http/Controllers/AuctionController.php
+++ app/Http/Controllers/AuctionController.php
@@ -37,31 +37,5 @@ class AuctionController extends Controller
       return view('pages.auctions');
     }
 
-    /**
-     * Creates a new card.
-     *
-     * @return Card The card created.
-     */
-    public function create(Request $request)
-    {
-      $card = new Card();
-
-      $this->authorize('create', $card);
-
-      $card->name = $request->input('name');
-      $card->user_id = Auth::user()->id;
-      $card->save();
-
-      return $card;
-    }
-
-    public function delete(Request $request, $id)
-    {
-      $card = Card::find($id);
-
-      $this->authorize('delete', $card);
-      $card->delete();
-
-      return $card;
-    }
+   
 }
